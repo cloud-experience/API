e3827aa31f023629685ef90c437f1f0c03f17116
Da-Taeho <78739301+Da-Taeho@users.noreply.github.com>
2021-05-09 14:49:08 +0900

    Initial commit

--- api_app.py
+++ api_app.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'url'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_DB'] = 'nuspamdb'
+app.config['MYSQL_DATABASE_HOST'] = 'database-2.cnr20hoyd3cu.ap-northeast-2.rds.amazonaws.com'
 
 mysql.init_app(app)
 
 @app.route('/')
 def get():
     cur = mysql.connect().cursor()
-    cur.execute('''select * from url.information''')
+    cur.execute('''select * from nuspamdb.urlinfo''')
     r = [dict((cur.description[i][0], value)
                 for i, value in enumerate(row)) for row in cur.fetchall()]
     return jsonify({'URL_INFORMATION' : r})
